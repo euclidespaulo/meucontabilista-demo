255ef2afa7ca083a5d94d89111606dce971968e4
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2023-12-29 08:40:03 -0500

    test branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
-import Foot from './components/Foot'
+import Home from './Components/Home'
+import Foot from './Components/Services'
 
 function App() {
   
