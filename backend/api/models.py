a87930733a6c63d5e67e3ec3388c3afc0099e72b
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2024-01-11 21:04:51 -0500

    model 4

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,64 +1,50 @@
 from django.db import models
 
-class Company(models.Model):
-    name =  models.CharField(max_length =100)
-    industry =  models.CharField(max_length =20)
-    location =  models.CharField(max_length =20)
-    
-    def __str__(self):
-        return self.name 
-
-class User(models.Model):
+class UserProfile(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     username = models.CharField(unique=True, max_length=100)
     password = models.CharField(max_length=100)
-    email = models.EmailField(max_length = 254)
     phone = models.CharField(max_length=100)
-    Rol = models.CharField(max_length=100)
-    company = models.ForeignKey(Company, on_delete = models.CASCADE)
+    role = models.CharField(max_length=100)
+    companyName = models.CharField(max_length=100)
+    industry = models.CharField(max_length=20)
     
     def __str__(self):
-        return self.username 
+        return self.username
 
-class Appointment(models.Model):
-    date = models.DateField()
-    time = models.TimeField()
-    
- 
-    opt = [
-        ("opt1", "Scheduled"),
-        ("opt", "Completed"),
-        ("opt3", "Canceled"),
-    ]
-    status = models.CharField(
-        max_length=10,
-        choices=opt
-    )
-    
-    option = [
-        ("option1", "Zomm"),
-        ("option2","Office"),
-        ("option3", " Phone"),
-    ]
-    
-    location = models.CharField(
-        max_length=10, 
-        choices = option
-    )
-        
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return self.date
-    
 class Accounting(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    email = models.EmailField(max_length = 254)
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    
+
     def __str__(self):
-        return self.firstname
+        return self.username
+
+class Appointment(models.Model):
+    dateTime = models.DateTimeField()
     
-    
+    STATUS_CHOICES = [
+        ("Scheduled", "Scheduled"),
+        ("Completed", "Completed"),
+        ("Canceled", "Canceled"),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    LOCATION_CHOICES = [
+        ("Zoom", "Zoom"),
+        ("Office", "Office"),
+        ("Phone", "Phone"),
+    ]
+    location = models.CharField(max_length=10, choices=LOCATION_CHOICES)
+
+    userProfile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    accounting = models.ForeignKey(Accounting, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.DateTime)
+
+
