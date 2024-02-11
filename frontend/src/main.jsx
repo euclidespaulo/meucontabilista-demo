5523eff4e5cf49e007f2f190c129af33c1011d2c
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2024-02-11 10:10:08 -0500

    I18next to tanslate EN - PT

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css';
+import './index.css'
 import App from './App.jsx'
 import {BrowserRouter as Router} from 'react-router-dom'
+import './Components/translation/i18n.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Router>
   <React.StrictMode>
+    <React.Suspense fallback="loading...">
       <App />
+    </React.Suspense>
   </React.StrictMode>
 </Router>
 );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,44 @@
-import { Stack } from "@mui/material";
-import HomeIntro from "../Components/common/homeIntro";
-import Section from '../Components/common/section'
-import sectionText from '../data/english/home/sectionText.json'
-import introText from '../data/english/home/introText.json'
-
+import React from 'react';
+import { Stack } from '@mui/material';
+import HomeIntro from '../Components/common/homeIntro';
+import Section from '../Components/common/section';
+import { useTranslation } from 'react-i18next';
+import introTextEN from '../data/locales/en/introText.json';
+import introTextPT from '../data/locales/pt/introText.json';
+import sectionTextEN from '../data/locales/en/sectionText.json';
+import sectionTextPT from '../data/locales/pt/sectionText.json';
 
 const Home = () => {
-  
+  const { t, i18n } = useTranslation();
+  const introText = i18n.language === 'pt' ? introTextPT : introTextEN;
+  const sectionText = i18n.language === 'pt' ? sectionTextPT : sectionTextEN;
+
+  // Convert the "br" object values into an array
+  const brList = Object.values(introText.br); // Set brList to an empty array if introText.br is undefined
+
+  const renderSection = (section) => {
+    // Render logic specific to sectionText structure
+    return (
+      <Section
+        title={t(section.title)}
+        description={t(section.description)}
+        imageSrc={t(section.imageSrc)}
+        details={Object.values(section.details).map((detail) => t(detail))}
+        reverse={section.reverse}
+      />
+    );
+  };
+
   return (
     <Stack direction='column'>
-      {
-        introText.map((introItem, index) => (
-            <HomeIntro key={index} {...introItem} />
-          ))
-      }
-
-      {
-        sectionText.map((sectionItem, index) => (
-            <Section key={index} {...sectionItem}/>
-          ))
-      }
-      
+      <HomeIntro
+        title={t(introText.title)}
+        br={brList.map((brItem) => t(brItem))}
+        description={t(introText.description)}
+        button={t(introText.button)}
+      />
+      {renderSection(sectionText['1'])}
+      {renderSection(sectionText['2'])}
     </Stack>
   );
 };
