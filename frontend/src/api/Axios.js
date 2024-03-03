efdf7fe805bec9ba721ed66431e517a03e588a50
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2024-03-02 19:21:13 -0500

    axions version

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const URL =  'http://127.0.0.1:8000/'
+
+const isDevelopment = import.meta.env.MODE === 'development';
+const  URL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD;
 
 const AxiosInstance = axios.create({
     baseURL: URL,
