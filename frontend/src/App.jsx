01c745021c49b425f364cba32a2c348493ff6c97
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2024-03-02 20:23:22 -0500

    deploy app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Book from './pages/Book';
-import Navbar from './components/common/Navbar';
+import Navbar from './components/common/NavBar';
 import Footer from './components/common/Footer';
 
 
